d5426c3c714e3d4762cbefee8c52662798aac993
ananndarios <52680405+ananndarios@users.noreply.github.com>
2020-06-30 20:02:06 -0300

    Funções map/filter json
    Arquivo com as funções para a busca dos arquivos no json dos videos.

---
+++ listadearquivos.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+function readdir(path){
+    return new Promise((resolve, reject)=>{
+        fs.readdir(path, (err,stat)=> {
+            if(err){
+                reject(err);
+            }else{
+                resolve(stat)
+            }
+        })
+    })
+}
+function stat(path){
+    return new Promise((resolve, reject)=>{
+        fs.stat(path, (err,stat)=>{
+            if(err){
+                reject(err);
+            }else{
+                resolve({path, stat})
+            }
+        })
+    })
+}
+// stat('./').then(stat=>{console.log(stat.isFile())})
+
+// readdir('./').then((paths)=> console.log(paths))
+
+async function lista(){
+    const paths = await readdir('./')
+    const statsPromises = paths.map(async(path)=> await stat(path))
+    const stats = await Promise.all(statsPromises)
+    const pathWithIsFile = stats.map(path => ({path: path.path, isFile: path.stat.isFile()}))
+    const files = pathWithIsFile.filter(path => path.isFile) 
+    console.log(files)
+}
+lista()
+
