11a34562fa15c1dff6d608b33553271d6481f8f5
rosi-souza <rosi.souza@live.com>
2020-07-01 16:26:59 -0300

    tela treinamento

---
+++ filtro.js
@@ -0,0 +1,9 @@
+const teste = data.video
+console.log(teste.filter((elem) => elem.id == 1))
+
+const filterItens = (qrcode) => { 
+  const filteredItens2 = menu.filter(element => element.id)
+  if (qrcode === element.id){ 
+    return filterItens2
+  }
+}
