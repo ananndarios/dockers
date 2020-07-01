8bfc02882451eeb65da833212398dc8d21fae795
rosi-souza <rosi.souza@live.com>
2020-07-01 18:07:36 -0300

    função de filtrar

--- filtro.js
+++ filtro.js
@@ -2,8 +2,6 @@ const teste = data.video
 console.log(teste.filter((elem) => elem.id == 1))
 
 const filterItens = (qrcode) => { 
-  const filteredItens2 = menu.filter(element => element.id)
-  if (qrcode === element.id){ 
-    return filterItens2
-  }
+  return filteredItens2 = menu.filter(element => element.id == qrcode)
+ 
 }
