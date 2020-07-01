401fe5fcad99280e6f909d0e5001fa32fb6748f7
jaquew <eng.jaquesilva@gmail.com>
2020-07-01 19:13:16 -0300

    pwa iniciado

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       '/home.html',
+       '/manifest.webmanifest',
+       '/media/icon.png',
+       '/media/wilson-sons-1.mp4'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
