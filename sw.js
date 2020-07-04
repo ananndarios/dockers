bea8ef2f75c7ad857d73c880f7f45aaae71edf4c
jaquew <eng.jaquesilva@gmail.com>
2020-07-04 15:52:05 -0300

    pwa corrigido

--- sw.js
+++ sw.js
@@ -1,18 +1,24 @@
+
 self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
-       '/src/home.html',
-       '/manifest.webmanifest',
-       '/media/icon.png',
-       '/media/wilson-sons-1.mp4'
-     ]);
+       'src/',
+       '/src/home.html',       
+       '/src/index.html',
+       '/src/index.css',
+       '/src/aulas.html',
+       '/src/treinamento.html',
+       '/media/'
+
+     ])
+     .then(() => console.log('Assets added to cache'))
+     .catch(err => console.log('Error while fetching assets', err));;
    })
  );
 });
 
 self.addEventListener('fetch', function(e) {
-  console.log(e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
